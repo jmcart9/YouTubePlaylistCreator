27c61e1975b936d999713d7409c443f9da0386e6
Jorvon Carter <carterjorvon@gmail.com>
2019-05-08 17:46:14 -0400

    test cases enhanced

--- src/main/java/test/TestGmailMethods.java
+++ src/main/java/test/TestGmailMethods.java
@@ -7,9 +7,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Base64;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.BeforeClass;
@@ -27,16 +25,108 @@ public class TestGmailMethods {
 	static Gmail service;
 	final String query = "from:noreply@youtube.com";;
 	final String userID = "thisistheforestprimeval@gmail.com";
-	final String messageID = "169eea3e4cfd169b";
+	final String messageID = "169c3b18a7d9dadb";
 	
 	Message message;
 	
 	@BeforeClass
 	public static void setUp() {
-		gmailMethods = new GmailMethods();
 		service = new Gmail.Builder(AuthGmail.HTTP_TRANSPORT, AuthGmail.JSON_FACTORY, AuthGmail.authorize())
 	            .setApplicationName("YouTube Playlist Creator")
 	            .build();
+		gmailMethods = new GmailMethods(service);
+	}
+	
+	//@Test
+	public void testGmailService() {
+		assertNotEquals(service, null);
+		assertEquals(service.getApplicationName(), "YouTube Playlist Creator");
+		assertNotNull(service.users());
+		System.out.println("service is okay, I think: " + service.toString());
+	}
+	
+	//@Test
+	public void testSetEmailMessageList() throws IOException {
+		
+		List<Message> list = gmailMethods.getEmailMessageList();
+		
+		assertNull(list);
+		System.out.println("unfilled email message list is null");
+		
+		System.out.println("filling now...");
+		gmailMethods.setEmailMessageList(service, userID, query);
+		list = gmailMethods.getEmailMessageList();
+	
+		assertNotNull(list);
+		System.out.println("now the email message list is not null");
+		
+		assertFalse(list.isEmpty());
+		System.out.println("EmailMessageList empty? " + list.isEmpty());
+	}
+	
+	//@Test
+	public void testSetEmailMessageListSIMPLE() throws IOException {
+		gmailMethods.setEmailMessageList(service, userID, query);
+		assertNotNull(gmailMethods.getEmailMessageList());
+		assertFalse(gmailMethods.getEmailMessageList().isEmpty());
+		
+		//System.out.println(gmailMethods.getEmailMessageList().get(2).toPrettyString() + "a");
+		//System.out.println(gmailMethods.getMessage("169c3b18a7d9dadb").toPrettyString() + "b");
+		
+	}
+	
+	//@Test
+	public void testGetEmailMessageList() {
+		List<Message> list = gmailMethods.getEmailMessageList();
+		
+		assertFalse(list.isEmpty());
+		System.out.println("EmailMessageList: " + list.toString());
+	}
+	
+	//@Test
+	public void testGetMessage() {
+		
+		message = gmailMethods.getMessage(messageID);
+		assertNotNull(message);
+		
+		//Files.writeString(Paths.get("output.txt"), m.toPrettyString());
+		
+	}
+	
+	//@Test
+	//@Test
+	public void testMessageBodyToString() {
+		message = gmailMethods.getMessage(messageID);
+		String out = gmailMethods.messageBodyToString(message);
+		
+		assertNotNull(out);
+		assertNotEquals(out,"");
+		System.out.println(out);
+	}
+	
+	//@Test
+	public void testGetVideoUrl() {
+		String s = gmailMethods.getVideoUrl(gmailMethods.messageBodyToString(gmailMethods.getMessage(messageID)));
+		assertNotNull(s);
+		assertNotEquals(s,"");
+		System.out.println(s);
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void testCreateVideoList() {
+		gmailMethods.setEmailMessageList(service, userID, query);
+		List list = gmailMethods.getEmailMessageList();
+		list.replaceAll(x -> gmailMethods.getVideoUrl(gmailMethods.messageBodyToString((Message)x)));
+
+		assertFalse(list.isEmpty());
+		assertNotNull(list);
+		list.forEach(System.out::println);
+	}
+	
+	//@Test
+	public void testgetVideoUrls(){
+		
 	}
 	
 	//@Test
@@ -54,81 +144,8 @@ public class TestGmailMethods {
 		assertEquals("EOBqpDcfVW0", gmailMethods.getVideoID(url5));
 	}
 	
-	//@Test
-	public void testGmailService() {
-		assertNotEquals(service, null);
-		assertEquals(service.getApplicationName(), "YouTube Playlist Creator");
-		assertNotNull(service.users());
-		System.out.println("service is okay, I think: " + service.toString());
-	}
-	
-	//@Test
-	public void testSetEmailMessageList() {
-		
-		List<Message> list = gmailMethods.getEmailMessageList();
-		
-		assertNull(list);
-		System.out.println("unfilled email message list is null");
-		
-		System.out.println("filling now...");
-		gmailMethods.setEmailMessageList(service, userID, query);
-		list = gmailMethods.getEmailMessageList();
-	
-		assertNotNull(list);
-		System.out.println("now the email message list is not null");
-		
-		assertFalse(list.isEmpty());
-		System.out.println("EmailMessageList empty?: " + list.isEmpty());
-	}
-	
-	@Test
-	public void testSetEmailMessageListSIMPLE() {
-		gmailMethods.setEmailMessageList(service, userID, query);
-		assertNotNull(gmailMethods.getEmailMessageList());
-		assertFalse(gmailMethods.getEmailMessageList().isEmpty());
-	}
-	
-	//@Test
-	public void testGetEmailMessageList() {
-		List<Message> list = gmailMethods.getEmailMessageList();
-		
-		assertFalse(list.isEmpty());
-		System.out.println("EmailMessageList: " + list.toString());
-	}
-	
-	//@Test
-	public void testGetMessage() {
-		
-		message = gmailMethods.getMessage(service, userID, messageID);
-		assertNotNull(message);
-		
-		//Files.writeString(Paths.get("output.txt"), m.toPrettyString());
-		
-	}
-	
-	@Test
-	public void testMessageBodyToString() {
-		message = gmailMethods.getMessage(service, userID, messageID);
-		String out = gmailMethods.messageBodyToString(message);
-		assertNotNull(out);
-		assertNotEquals(out,"");
-		System.out.println(out);
-	}
-	
-	//@Test
-	public void testGetVideoUrl() {
-		String s = gmailMethods.getVideoUrl(gmailMethods.messageBodyToString(gmailMethods.getMessage(service, userID, messageID)));
-		assertNotNull(s);
-		assertNotEquals(s,"");
-		System.out.println(s);
-	}
-	
 	public static void main(String... args) {
 		//setUp();
-		//testSetEmailMessageList();
-		
+		//testCreateVideoList();
 	}
-
-
-	
 }
