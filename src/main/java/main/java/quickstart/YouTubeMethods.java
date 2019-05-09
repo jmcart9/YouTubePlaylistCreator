22bee1185f5204854a6a460f379c76ad8e814c39
Jorvon Carter <carterjorvon@gmail.com>
2019-05-09 15:34:13 -0400

    Youtube methods cleaned up and simplified

--- src/main/java/main/java/quickstart/YouTubeMethods.java
+++ src/main/java/main/java/quickstart/YouTubeMethods.java
@@ -24,83 +24,11 @@ import java.util.Calendar;
 import java.util.List;
 
 public class YouTubeMethods {
-
-    /** Application name. */
-    private static final String APPLICATION_NAME = "YouTube Playlist Creator";
-
-    /** Directory to store user credentials for this application. */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(
-        System.getProperty("user.home"), ".credentials/youtube-java-quickstart");
-
-    /** Global instance of the {@link FileDataStoreFactory}. */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /** Global instance of the JSON factory. */
-    private static final JsonFactory JSON_FACTORY =
-        JacksonFactory.getDefaultInstance();
-
-    /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT;
-    
-    /** Global instance of the scopes required by this quickstart.
-    *
-    * If modifying these scopes, delete your previously saved credentials
-    * at ~/.credentials/drive-java-quickstart
-    */
-   private static final List<String> SCOPES =
-       Arrays.asList(YouTubeScopes.YOUTUBE_READONLY);
-
-   static {
-       try {
-           HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-           DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-       } catch (Throwable t) {
-           t.printStackTrace();
-           System.exit(1);
-       }
-   }
-   
-   /**
-    * Create an authorized Credential object.
-    * @return an authorized Credential object.
-    * @throws IOException
-    */
-   public static Credential authorize() throws IOException {
-       // Load client secrets.
-       InputStream in =
-           Quickstart.class.getResourceAsStream("/client_secret.json");
-       GoogleClientSecrets clientSecrets =
-           GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-       // Build flow and trigger user authorization request.
-       GoogleAuthorizationCodeFlow flow =
-               new GoogleAuthorizationCodeFlow.Builder(
-                       HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-               .setDataStoreFactory(DATA_STORE_FACTORY)
-               .setAccessType("offline")
-               .build();
-       Credential credential = new AuthorizationCodeInstalledApp(
-           flow, new LocalServerReceiver()).authorize("user");
-       return credential;
-   }
-	
-    /**
-     * Build and return an authorized API client service, such as a YouTube
-     * Data API client service.
-     * @return an authorized API client service
-     * @throws IOException
-     */
-    public static YouTube getYouTubeService() throws IOException {
-        Credential credential = authorize();
-        return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
     
     /**
      * Create a playlist and add it to the authorized account.
      */
-    static String insertPlaylist(YouTube youtube, String title) throws IOException {
+    public static String insertPlaylist(YouTube youtube, String title) throws IOException {
 
         // This code constructs the playlist resource that is being inserted.
         // It defines the playlist's title, description, and privacy status.
---
+++ src/main/java/test/TestYouTubeMethods.java
@@ -0,0 +1,48 @@
+package test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.services.youtube.YouTube;
+import com.google.common.collect.Lists;
+
+import main.java.quickstart.AuthYouTube;
+import main.java.quickstart.YouTubeMethods;
+
+
+
+public class TestYouTubeMethods {
+	
+	static YouTubeMethods youtubeMethods;
+	static YouTube service;
+	
+	@BeforeClass
+	public static void setUp() {
+		List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube");
+		try {
+			Credential credential = AuthYouTube.authorize(scopes, "StoredCredentialYoutube");
+			service = new YouTube.Builder(AuthYouTube.HTTP_TRANSPORT, AuthYouTube.JSON_FACTORY, credential)
+	                .setApplicationName("YouTube Playlist Creator")
+	                .build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		youtubeMethods = new YouTubeMethods();
+	}
+	
+	@Test
+	public void testInsertPlaylist() {
+		try {
+			String x = youtubeMethods.insertPlaylist(service, "testy6 ");
+			System.out.println(x);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
