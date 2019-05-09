2d99916a21eb21eb0db3d8a1aa2eb00a4822597e
Jorvon Carter <carterjorvon@gmail.com>
2019-05-09 17:14:34 -0400

    minor edit

--- src/main/java/main/java/quickstart/YouTubeMethods.java
+++ src/main/java/main/java/quickstart/YouTubeMethods.java
@@ -1,28 +1,16 @@
 package main.java.quickstart;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-
-import com.google.api.services.youtube.YouTubeScopes;
-import com.google.api.services.youtube.model.*;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.youtube.YouTube;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.List;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Playlist;
+import com.google.api.services.youtube.model.PlaylistItem;
+import com.google.api.services.youtube.model.PlaylistItemSnippet;
+import com.google.api.services.youtube.model.PlaylistSnippet;
+import com.google.api.services.youtube.model.PlaylistStatus;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.VideoListResponse;
 
 public class YouTubeMethods {
     
